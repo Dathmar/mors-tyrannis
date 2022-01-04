c718bd867d3fe8ee474219d1591b9948c714be28
Asher Danner <adanner@ets.org>
2022-01-04 07:44:38 -0600

    Made description a text area, fixed is_private boolean name, added require join approval.

--- communities/forms.py
+++ communities/forms.py
@@ -1,17 +1,26 @@
 from django import forms
-from django_markdown.widgets import MarkdownWidget
 
 
 class CommentForm(forms.Form):
-    content = forms.CharField(widget=MarkdownWidget())
+    content = forms.CharField()
 
 
 class CommunityForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    description = forms.CharField(widget=MarkdownWidget())
-    is_public = forms.BooleanField(required=False)
+    name = forms.CharField(max_length=100, required=True)
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'size-form-textarea', 'placeholder': 'Community description',
+                                     'rows': '2', 'style': 'width:100%;'}), required=True)
+    is_private = forms.BooleanField(required=False)
+    require_join_approval = forms.BooleanField(required=False)
+
+    def clean_name(self):
+        reserved_community_names = ['create', ]
+
+        if self.cleaned_data['name'] in reserved_community_names:
+            raise forms.ValidationError('This name is reserved.')
+        return self.cleaned_data['name']
 
 
 class PostForm(forms.Form):
-    content = forms.CharField(widget=MarkdownWidget())
+    content = forms.CharField()
 
