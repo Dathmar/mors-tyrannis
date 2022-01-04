7b1964f7b91e1f07d0236c64cbbab35078fa9de2
Asher Danner <adanner@ets.org>
2022-01-04 07:43:07 -0600

    Added create community

--- communities/urls.py
+++ communities/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
-
 app_name = 'communities'
+
 urlpatterns = [
-    path('', views.index, name='community-list'),
-    path('<slug:slug>/', views.community, name='community'),
+    path('', views.index, name='list'),
+    path('create/', views.CreateView.as_view(), name='create'),
+    path('<slug:community_slug>/', views.community, name='detail'),
     # path('ship-create-view/', views.CreateView.as_view(), name='ship-create-view'),
 ]
