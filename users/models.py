91c4297cbb24a15dc29ca1df3082fbd4746e952e
Asher Danner <adanner@ets.org>
2022-01-18 10:42:50 -0600

    Better model use

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 # Create your models here.
 class UserMeta(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     reputation = models.IntegerField(default=0)
 
     def __str__(self):
