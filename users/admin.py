f9cd88f6f63ef0f2e29f8a88905e7020163660b0
Asher Danner <adanner@ets.org>
2022-01-18 10:43:15 -0600

    Add UserMeta

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UserMeta
+
 
 # Register your models here.
+@admin.register(UserMeta)
+class UserMetaAdmin(admin.ModelAdmin):
+    list_display = ('user', 'reputation')
+    list_editable = ('reputation',)
+    search_fields = ('user',)
+    list_filter = ('reputation',)
