1c87f6372256b76c7ccb34efa4287186b896996a
Asher Danner <adanner@ets.org>
2022-01-31 13:56:42 -0600

    Complete initial join community code.  Need to think about how the flow should really work.

--- communities/static/js/join-community.js
+++ communities/static/js/join-community.js
@@ -10,10 +10,10 @@ function join_attempt(community_slug) {
         success: function(data) {
             if (data.success) {
                 let join_button = $('#join_community_button');
-                console.log(join_button);
-                let join_parent = join_button.closest('div');
-                console.log(join_parent);
-                join_parent.innerHTML = '<a href="/c/" + community_slug + "/new-post/" class="btn btn-primary rounded-3 mt-3" style="width: 11em;">Create Post</a>';
+                let join_parent = join_button.closest('div')
+                let url = "/c/" + community_slug + "/new-post/"
+                join_parent.html('<a href=' + url + ' class="btn btn-primary rounded-3 mt-3" style="width: 11em;">Create Post</a>');
+
             } else {
                 console.log('failure');
             }
