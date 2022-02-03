8935eb5357731b11e79a421eaf941ed67590c423
Asher Danner <adanner@ets.org>
2022-02-03 13:12:19 -0600

    Add edit option and edit date when post is edited

--- communities/urls.py
+++ communities/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('<slug:community_slug>/verify-join/', views.verify_join, name='join-verify'),
     path('<slug:community_slug>/new-post/', views.CreatePostView.as_view(), name='create-post'),
     path('<slug:community_slug>/comments/<int:post_id>/', views.post_view, name='view-post'),
+    path('<slug:community_slug>/comments/<int:post_id>/edit/', views.EditPostView.as_view(), name='edit-post'),
     path('<slug:community_slug>/comments/<int:post_id>/add-comment/', views.AddCommentView.as_view(), name='post-add-comment'),
     path('<slug:community_slug>/comments/<int:post_id>/upvote/', views.upvote_post_comment, name='upvote-post'),
     path('<slug:community_slug>/comments/<int:post_id>/downvote/', views.downvote_post_comment, name='downvote-post'),
