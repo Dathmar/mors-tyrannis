a1d3afabeb6d81e0118af1524a8878ba3fc4049c
Asher Danner <adanner@ets.org>
2022-02-04 12:05:17 -0600

    If user isn't logged in display login prompt and no posts.

--- base/views.py
+++ base/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 
-from communities.models import Post
+from communities.models import Post, CommunityMember
 
 
 def index(request):
-    posts = Post.objects.all()[:20]
+    posts = None
+    if request.user.is_authenticated:
+        communities = CommunityMember.objects.filter(user=request.user).values_list('community', flat=True)
+        posts = Post.objects.filter(community__in=communities)[:20]
     return render(request, 'base/index.html', {'posts': posts})
