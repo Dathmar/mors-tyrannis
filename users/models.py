752e8416b81ad5b686f09063dcd10ceb4543a731
Asher Danner <adanner@ets.org>
2022-02-05 10:13:33 -0600

    Add default to last notification

--- users/models.py
+++ users/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.conf import settings
-
+from django.utils.timezone import now
 
 # Create your models here.
 class UserMeta(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     reputation = models.IntegerField(default=0)
-    last_notification_check = models.DateTimeField(auto_now_add=True)
+    last_notification_check = models.DateTimeField(default=now())
 
     def __str__(self):
         return self.user.username
