840b34276731784f0f7f3a0ca6c437f5e166d6d9
Asher Danner <adanner@ets.org>
2022-02-04 20:47:21 -0600

    Add following to filter for index page.

--- base/views.py
+++ base/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.views import View
 
 from communities.models import Post, CommunityMember
 
@@ -7,6 +6,6 @@ from communities.models import Post, CommunityMember
 def index(request):
     posts = None
     if request.user.is_authenticated:
-        communities = CommunityMember.objects.filter(user=request.user).values_list('community', flat=True)
+        communities = CommunityMember.objects.filter(user=request.user, following=True).values_list('community', flat=True)
         posts = Post.objects.filter(community__in=communities)[:20]
     return render(request, 'base/index.html', {'posts': posts})
